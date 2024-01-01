6dda2a2371997e424c8c944e3bd6e7ef879c3331
kingvpn77 <155173495+king-vpn@users.noreply.github.com>
2024-01-01 17:13:17 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -328,8 +328,8 @@ restart_system() {
 <code>Ip vps : </code><code>$ipsaya</code>
 <code>Exp Sc : </code><code>$EXPSC</code>
 <code>────────────────────</code>
-<i>Automatic Notification from Github</i>
-"'&reply_markup={"inline_keyboard":[[{"text":"⭐ᴏʀᴅᴇʀ⭐","url":"https://t.me/king-vpn"},{"text":"⭐ɪɴꜱᴛᴀʟʟ⭐","url":"https://wa.me/6287846867519"}]]}'
+<i>Automatic Notification from kingvpn77</i>
+"'&reply_markup={"inline_keyboard":[[{"text":"⭐ᴏʀᴅᴇʀ⭐","url":"https://t.me/kingvpn77"},{"text":"⭐ɪɴꜱᴛᴀʟʟ⭐","url":"https://wa.me/6287846867519"}]]}'
     curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 }
 clear
